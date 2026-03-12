44ec1f2a83ba87891ade9311dac4cc6fe29dc0b6
aliatwood <aliatwood0104@gmail.com>
2026-03-12 18:14:13 +0100

    Uppdaterade header
    La till en meny i heaader.html, styleade header i header.scss och la till lite javascript kod som markerar vilken sida man e på med hjälp av active klassen

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import "./styles/styles.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+    let links = document.querySelectorAll("nav a");
+    for (let i = 0; i < links.length; i++) {
+        if (links[i].href === window.location.href){
+            links[i].classList.add("active");
+        }
+        else{
+            links[i].classList.remove("active");
+        }
+    }    
+})

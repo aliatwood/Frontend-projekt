906e96ceb65362685bfec262e09bae95816cd774
aliatwood <aliatwood0104@gmail.com>
2026-03-16 21:08:04 +0100

    uppdatera vite.config.js
    La till base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+    base: '/Frontend-projekt/',
     root: "src",
     build: {
         outDir: "../docs",

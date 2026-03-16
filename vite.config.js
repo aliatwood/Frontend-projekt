452083e18be94a921173ec79dd34792257ed859f
aliatwood <aliatwood0104@gmail.com>
2026-03-16 21:04:11 +0100

    uppdatera vite.config.js och körde npm run build
    La till input till varje html i vite.config.js och körde npm run build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
     root: "src",
     build: {
         outDir: "../docs",
         emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                utforska: resolve(__dirname, 'src/utforska.html'),
+                favoriter: resolve(__dirname, 'src/favoriter.html'),
+                'om-oss': resolve(__dirname, 'src/om-oss.html'),
+            }
+        }
     }
 })
